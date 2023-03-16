252749df0f2fb3ee44428c05ae00d2d00f881795
Kaoru <jacobyb0508@gmail.com>
2023-03-16 19:47:39 -0400

    Fixed issues

--- Loadstring.lua
+++ Loadstring.lua
@@ -2,10 +2,6 @@ if isfile("AlphaZero/Loader.lua") then
     loadfile("AlphaZero/Loader.lua")();
 else
     local Status, Script = pcall(game.HttpGet, game, "https://raw.githubusercontent.com/Sw1ndlerScripts/AlphaZero/main/Loader.lua");
-if isfile("AlphaZero/Loader.lua") then
-    loadfile("AlphaZero/Loader.lua")();
-else
-    local Status, Script = pcall(game.HttpGet, game, "https://raw.githubusercontent.com/Uvxtq/AlphaZero/main/Loader.lua");
 
     if Status then
         loadstring(Script)()
