f3afa0a1bbd2decd93078cebb20b231ed21cc846
Alex <103692349+Sw1ndlerScripts@users.noreply.github.com>
2023-04-04 17:46:18 -0700

    Update PlaceIds.lua

--- Games/PlaceIds.lua
+++ Games/PlaceIds.lua
@@ -1,7 +1,7 @@
 return {
     ["Weaponry"] = 3297964905;
     ["Pistol-1v1"] = 6722284015;
-    ["Basically FNF"] = 6520999642;
-    ["Project New World"] = 6918802270;
-    ["Tower Of Hell"] = 1962086868;
+    ["BasicallyFNF"] = 6520999642;
+    ["ProjectNewWorld"] = 6918802270;
+    ["TowerOfHell"] = 1962086868;
 };
